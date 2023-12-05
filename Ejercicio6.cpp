39c6916eead297afd5806dbed3604f781086fabb
Jose Jurado Cortés <jjuracor@myuax.com>
2023-12-05 11:35:11 +0100

    ej 6 prueba 2

--- Ejercicio6.cpp
+++ Ejercicio6.cpp
@@ -27,3 +27,26 @@ public:
     void addSymbol(const std::string& symbol, const Variant::Value& value) {
         symbolTable[symbol] = value;
     }
+    Variant::Value getSymbolValue(const std::string& symbol) const {
+        auto it = symbolTable.find(symbol);
+        if (it != symbolTable.end()) {
+            return it->second;
+        } else {
+            std::cerr << "Error: El símbolo '" << symbol << "' no está definido." << std::endl;
+            return Variant::Value{};
+        }
+    }
+
+    void removeSymbol(const std::string& symbol) {
+        auto it = symbolTable.find(symbol);
+        if (it != symbolTable.end()) {
+            symbolTable.erase(it); // Eliminar el símbolo si existe
+            std::cout << "Símbolo '" << symbol << "' eliminado del entorno." << std::endl;
+        } else {
+            std::cerr << "Error: El símbolo '" << symbol << "' no existe en el entorno." << std::endl;
+        }
+    }
+
+    bool symbolExists(const std::string& symbol) const {
+        return symbolTable.find(symbol) != symbolTable.end();
+    }
