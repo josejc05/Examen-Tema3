962ebd5ec0654c644d92d6ad37eed00f8e98a51c
Jose Jurado Cortés <jjuracor@myuax.com>
2023-12-05 10:40:56 +0100

    ej 1 prueba 2

--- Ejercicio1.cpp
+++ Ejercicio1.cpp
@@ -14,3 +14,17 @@ public:
     void addSymbol(const std::string& name, int value) {
         symbolTable[name] = value;
     }
+    // Función para obtener el valor de un símbolo desde el entorno
+    int getSymbolValue(const std::string& name) {
+        // Si el símbolo no está presente, devuelve un valor predeterminado (puedes ajustarlo según tu necesidad)
+        if (symbolTable.find(name) != symbolTable.end()) {
+            return symbolTable[name];
+        } else {
+            std::cerr << "Error: Symbol '" << name << "' not found in the environment." << std::endl;
+            return 0; // Puedes cambiar esto según tus necesidades
+        }
+    }
+
+private:
+    std::map<std::string, int> symbolTable;
+};
