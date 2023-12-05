fa2a79463e1a913f1849c1aef819ac4c27344628
Jose Jurado Cortés <jjuracor@myuax.com>
2023-12-05 11:07:10 +0100

    ej 3 prueba 2

--- Ejercicio3.cpp
+++ Ejercicio3.cpp
@@ -16,3 +16,19 @@ public:
     void insert(const string& name, int value) {
         symbolTable[name] = value;
     }
+    // Método para obtener el valor de un símbolo desde el entorno
+    int lookup(const string& name, int defaultValue = 0) {
+        // Buscar el símbolo en el entorno
+        auto it = symbolTable.find(name);
+        if (it != symbolTable.end()) {
+            // Si el símbolo existe, devolver su valor
+            return it->second;
+        } else {
+            // Si el símbolo no existe, devolver el valor predeterminado
+            return defaultValue;
+        }
+    }
+
+private:
+    map<string, int> symbolTable;
+};
