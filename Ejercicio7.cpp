aef5e58185044a8fa51993004a84cf2a90a3089c
Jose Jurado Cortés <jjuracor@myuax.com>
2023-12-05 11:43:42 +0100

    ej 7 prueba 2

--- Ejercicio7.cpp
+++ Ejercicio7.cpp
@@ -21,3 +21,24 @@ public:
 private:
     Value value;
 };
+class Environment {
+public:
+    // Función para establecer una configuración en el entorno
+    void setConfiguration(const std::string& key, const Variant::Value& value) {
+        configTable[key] = value;
+    }
+
+    // Función para obtener el valor de una configuración desde el entorno
+    Variant::Value getConfiguration(const std::string& key) const {
+        auto it = configTable.find(key);
+        if (it != configTable.end()) {
+            return it->second;
+        } else {
+            std::cerr << "Error: Configuración '" << key << "' no encontrada." << std::endl;
+            return Variant::Value{};
+        }
+    }
+
+private:
+    std::map<std::string, Variant::Value> configTable;
+};
