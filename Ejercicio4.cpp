30fd4e8aa80168753b439550a3b3e244ffa17473
Jose Jurado Cortés <jjuracor@myuax.com>
2023-12-05 11:16:24 +0100

    ej 4 prueba 2

--- Ejercicio4.cpp
+++ Ejercicio4.cpp
@@ -24,3 +24,25 @@ public:
             symbolTable[name] = value;
         }
     }
+    // Método para obtener el valor de un símbolo desde el entorno
+    int lookup(const string& name, int defaultValue = 0) {
+        try {
+            // Buscar el símbolo en el entorno
+            auto it = symbolTable.find(name);
+            if (it != symbolTable.end()) {
+                // Si el símbolo existe, devolver su valor
+                return it->second;
+            } else {
+                // Si el símbolo no existe, lanzar una excepción
+                throw out_of_range("Error: Symbol '" + name + "' not found in the environment.");
+            }
+        } catch (const exception& e) {
+            cerr << e.what() << endl;
+            // Devolver el valor predeterminado en caso de excepción
+            return defaultValue;
+        }
+    }
+
+private:
+    map<string, int> symbolTable;
+};
